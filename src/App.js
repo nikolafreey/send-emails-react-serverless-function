36ee4eaa7e689381acbaf9fa7742873cbb422909
Nikola Vukovic <nikolav54@gmail.com>
2021-09-03 10:40:53 +0200

    Finnished form that we'll use to send the email with full UX features validation, snackbar and MaterialUI Elements

--- src/App.js
+++ src/App.js
@@ -131,14 +131,180 @@ function App() {
       value: event.target.value,
       valid: checkValidity(
         event.target.value,
-        contactForm[inputChangedHandler].validation
+        contactForm[inputIdentifier].validation
       ),
       blur: event.type == "blur" ? true : false,
       touched: true,
     };
+
+    //create a new object represeting the whole form object
+    const updatedContactForm = {
+      ...contactForm,
+      [inputIdentifier]: updatedFormElement,
+    };
+
+    //the whole form is valid until it's not
+    let formIsValid = true;
+    for (let inputElementIdentifier in updatedContactForm) {
+      formIsValid =
+        updatedContactForm[inputElementIdentifier].valid && formIsValid;
+    }
+
+    //update `contactForm` state
+    setContactForm(updatedContactForm);
+
+    //update `formIsValid` state
+    setFormIsValid(formIsValid);
   };
 
-  return <div></div>;
+  const checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if (rules.required) {
+      //value.trim() gets rid of whitespace
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.isEmail) {
+      //the `pattern` constant is a Regular Expression that matches the shape of an email
+      const pattern =
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+      //`pattern.test()` returns true or false
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  };
+
+  //this function is called when the user closes the snackbar after getting an error (when the cloud function fails)
+  const closeSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
+  //this function is called when clicking the `Send` button
+  const submitForm = () => {
+    //if a bot filled the honeypot field, don't keep running code (e.g don't call a cloud function)
+    if (contactForm.honeypot.value != "") {
+      //the below `return` is called an `early return` and it ends the execution of the function.
+      return;
+    }
+
+    //set `isLoading` state to true, so the spinner is rendered
+    setIsLoading(true);
+
+    //this fake cloud function consoles log the data from the form that is passed to it, and has a 50% chance of resolving.
+    const fakeCloudFunction = (data) => {
+      console.log(data);
+      return new Promise((resolve, reject) => {
+        const error = Math.random() > 0.5 ? true : false;
+        setTimeout(() => {
+          if (!error) {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 1000);
+      });
+    };
+
+    //call the fake cloud function. Later on, this function will be replaced by the real cloud function.
+    fakeCloudFunction({
+      name: contactForm.name.value,
+      email: contactForm.email.value,
+      message: contactForm.message.value,
+    })
+      //the code inside the `then()` block runs when the message was successfully sent from inside the cloud function
+      .then(() => {
+        let originalContactForm = {};
+        for (let key in contactForm) {
+          originalContactForm[key] = {
+            ...contactForm[key],
+            value: "",
+            valid: key == "honeypot" ? true : false,
+            blur: false,
+          };
+        }
+
+        //reset `contactForm` state to its original value
+        setContactForm(originalContactForm);
+
+        //reset the whole form validity to false
+        setFormIsValid(false);
+
+        //set `error` state to false.
+        setError(false);
+
+        //set `isLoading` state to false, so the spinner is not rendered anymore
+        setIsLoading(false);
+
+        //set `openSnackbar` state to true, so the snackbar is rendered, with content that depends on the error state
+        setOpenSnackbar(true);
+      })
+      //this code below runs when the message was NOT successfully sent from inside of the cloud function
+      .catch(() => {
+        //set `error` state to true
+        setError(true);
+
+        //set `isLoading` state to false, so the spinner is not rendered anymore
+        setIsLoading(false);
+
+        //set `openSnackbar` state to true, so the snackbar is rendered, with content that depends on the error state
+        setOpenSnackbar(true);
+      });
+  };
+
+  return (
+    <Box mt="3rem">
+      <Grid container justify="center">
+        <Box>
+          <Grid container alignItems="center" direction="column">
+            <h2>Send us a message</h2>
+            <p>We'll get back to you soon.</p>
+            <form>
+              {formElements}
+              <Grid container justify="center">
+                <Box mt="2rem">
+                  {isLoading ? (
+                    <CircularProgress />
+                  ) : (
+                    <Button
+                      onClick={submitForm}
+                      disabled={!formIsValid}
+                      variant="contained"
+                      color="primary"
+                    >
+                      Send
+                    </Button>
+                  )}
+                </Box>
+              </Grid>
+            </form>
+          </Grid>
+        </Box>
+      </Grid>
+      {error ? (
+        <Snackbar
+          open={openSnackbar}
+          onClose={closeSnackbar}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        >
+          <Alert onClose={closeSnackbar} severity="error">
+            Oops! Something went wrong, try again later.
+          </Alert>
+        </Snackbar>
+      ) : (
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={2000}
+          onClose={closeSnackbar}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        >
+          <Alert severity="success">Message sent!</Alert>
+        </Snackbar>
+      )}
+    </Box>
+  );
 }
 
 export default App;
